2ffbc9dda274b18159d0ad1ddf0f25854a390fc9
avish2687 <avishkar.kumar555@gmail.com>
2026-03-08 21:27:29 +0530

    updated

--- Restaurant-Management-System/backend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Chef, Testimonial, MenuItem, Booking, ContactMessage
-
-admin.site.register(Chef)
-admin.site.register(Testimonial)
-admin.site.register(MenuItem)
-admin.site.register(Booking)
-admin.site.register(ContactMessage)
